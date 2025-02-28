6eb1f26d5f8ab0408dee9c3e67ed79f8104fd8c3
Daniel Enrico <43122682+thedanielenrico@users.noreply.github.com>
2025-02-27 16:48:59 -0800

    Dynamic client data (#1)
    * add input field and query based on input
    
    * remove children on submit if they exist

--- apps1.js
+++ apps1.js
@@ -2,36 +2,54 @@
 const output = document.querySelector(".output");
 const url = "https://docs.google.com/spreadsheets/d/";
 const ssid = "1rl777z7NhjirDUxx1HgHbLmx58AogsR6o5qQx8ZmRpw";
-const query1 = `/gviz/tq?`;
+const query1 = `/gviz/tq?gid=1904945682`;
 
-const endpoint = `${url}${ssid}${query1}`;
+const clientIdSubmit = document.getElementById("client-id-submit-btn");
+const clientIdInput = document.getElementById("client-id");
 
-fetch(endpoint)
-  .then((res) => res.text())
-  .then((data) => {
-    const temp = data.substring(47).slice(0, -2);
-    const json = JSON.parse(temp);
-    const rows = json.table.rows;
-    const cols = json.table.cols;
+clientIdSubmit.addEventListener("click", () => {
+  const clientIdVal = clientIdInput.value;
+  if (clientIdVal.length >= 1) {
+    fetchClientData(clientIdVal);
+  }
+});
 
-    cols.forEach((col) => {
-      if (col.label !== "Progress Bar") {
-        const colHeader = document.createElement("th");
-        colHeader.textContent = col.label;
-        output.append(colHeader);
-      }
-    });
-    rows.forEach((row) => {
-      const div = document.createElement("tr");
-      const temp1 = row.c;
-      temp1.forEach((cell) => {
-        if (cell) {
-          const box = document.createElement("td");
-          box.textContent = cell.v;
-          box.classList.add("box");
-          div.append(box);
-        }
+function fetchClientData(clientId) {
+  if (clientId) {
+    const endpoint = `${url}${ssid}/gviz/tq?gid=${clientId}`;
+    if (output.hasChildNodes()) {
+      output.innerHTML = "";
+    }
+
+    fetch(endpoint)
+      .then((res) => res.text())
+      .then((data) => {
+        const temp = data.substring(47).slice(0, -2);
+        const json = JSON.parse(temp);
+        const rows = json.table.rows;
+        const cols = json.table.cols;
+        console.log(json);
+        cols.forEach((col) => {
+          if (col.label !== "Progress Bar") {
+            const colHeader = document.createElement("th");
+            colHeader.textContent = col.label;
+            output.append(colHeader);
+          }
+        });
+        rows.forEach((row) => {
+          const tableRow = document.createElement("tr");
+          const rowContent = row.c;
+          rowContent.forEach((cell) => {
+            if (cell) {
+              const tableData = document.createElement("td");
+              tableData.textContent = cell.v;
+              tableData.classList.add("box");
+              tableRow.append(tableData);
+            }
+          });
+          output.append(tableRow);
+          clientIdInput.value = "";
+        });
       });
-      output.append(div);
-    });
-  });
+  }
+}
